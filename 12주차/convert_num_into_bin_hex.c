5eeedc2d7736aabdb1464ca40664535916d74879
jinnyfruit <jinnyfruit0527@gmail.com>
2020-11-25 02:01:16 +0900

    SWD code

--- 12주차/convert_num_into_bin_hex.c
+++ 12주차/convert_num_into_bin_hex.c
@@ -3,19 +3,21 @@ file name: converting decimal number to binary string and hezadecimal string
 author: Kim, Ji Woo
 */
 #include <stdio.h>
-#define MASK 0x08
 
 void bin(int);
 void hex(int*);
-
-int i=0,arr[100];
+#define MAX 10000
+int i=0,arr[MAX];
 
 int main(){
-    int num=254;
-    printf("%d\n",num);
-    bin(num);
-    printf("\n");
-    printf("0x%x\n");
+    int i;
+
+    for(i=1;i<=500;i++){
+        printf("DEC %d\t",i);
+        printf("BIN ");
+        bin(i);
+        printf("\tHEX %x\n",i);
+    }
 
     return 0;
 }
@@ -32,4 +34,7 @@ void bin(int num) {
         i++;
         arr[i]=num%2;
 	}
+}
+void hex(int* arr){ //커찮
+
 }
