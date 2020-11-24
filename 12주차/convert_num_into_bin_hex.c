9339fa2122df0192fb1a5bbef3f2480115d43e85
jinnyfruit <jinnyfruit0527@gmail.com>
2020-11-24 09:06:48 +0900

    last code

--- 12주차/convert_num_into_bin_hex.c
+++ 12주차/convert_num_into_bin_hex.c
@@ -6,23 +6,30 @@ author: Kim, Ji Woo
 #define MASK 0x08
 
 void bin(int);
-void hex(int);
+void hex(int*);
+
+int i=0,arr[100];
 
 int main(){
-    int num=1000;
+    int num=254;
     printf("%d\n",num);
     bin(num);
     printf("\n");
-    printf("0x%x",num);
+    printf("0x%x\n");
 
     return 0;
 }
 void bin(int num) {
-	if (num == 0 || num == 1)
+	
+    if (num == 0 || num == 1){
 		printf("%d", num);
+        arr[i]=num;
+    }
+
 	else {
 		bin(num / 2);
 		printf("%d", num % 2);
+        i++;
+        arr[i]=num%2;
 	}
 }
-void hex()
